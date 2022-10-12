4af11e08e3acbaf3305bbd4dc9fe5fe469d0e8c2
kothale19 <kothale.pujaprasad@gmail.com>
2022-10-12 22:30:23 +0530

    Added: Monthly Wage computation logic

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -6,25 +6,33 @@ public class EmployeeWage {
 	static final int Wage_Per_Hour = 20;
 	static final int Full_Day_Hour = 8;
 	static final int Part_Time_Hour = 4;
+	static final int Working_Day_Per_Month = 20;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program!!");
-		int attendance = (int) (Math.floor(Math.random() * 10)) % 3;
-		switch (attendance) {
-		case Is_Present:
-			System.out.println("Employee is working Fulltime");
-			int dailyWage = Wage_Per_Hour * Full_Day_Hour;
-			System.out.println("Employee daily wage is:" + dailyWage);
-			break;
-		case Is_Parttime:
-			System.out.println("Employee is working Parttime");
-			dailyWage = Wage_Per_Hour * Part_Time_Hour;
-			System.out.println("Employee daily Wage is:" + dailyWage);
-			break;
-		default:
-			System.out.println("Employee is absent");
+		int dailyWage = 0;
+		int monthlyWage = 0;
+		for (int day = 1; day <= Working_Day_Per_Month; day++) {
 
+			int attendance = (int) (Math.floor(Math.random() * 10)) % 3;
+			switch (attendance) {
+			case Is_Present:
+				System.out.println("Employee is working Fulltime");
+				dailyWage = Wage_Per_Hour * Full_Day_Hour;
+				System.out.println("Employee daily wage is:" + dailyWage);
+				break;
+			case Is_Parttime:
+				System.out.println("Employee is working Parttime");
+				dailyWage = Wage_Per_Hour * Part_Time_Hour;
+				System.out.println("Employee daily Wage is:" + dailyWage);
+				break;
+			default:
+				System.out.println("Employee is absent");
+
+			}
+			monthlyWage += dailyWage;
 		}
-	}
+		System.out.println("monthlyWage is: " + monthlyWage);
 
+	}
 }
