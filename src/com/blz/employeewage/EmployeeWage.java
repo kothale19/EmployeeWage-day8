904c146bd4d1a548629740655a27ba95641e4c3e
kothale19 <kothale.pujaprasad@gmail.com>
2022-10-12 17:55:37 +0530

    Added: Employee DailyWage Computation Logic

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -2,14 +2,19 @@ package com.blz.employeewage;
 
 public class EmployeeWage {
 	static final int Is_Present = 1;
+	static final int Wage_Per_Hour = 20;
+	static final int Full_Day_Hour = 8;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program!!");
 		int attendance = (int) (Math.floor(Math.random() * 10)) % 2;
 		System.out.println(attendance);
-		if (attendance == Is_Present)
+		if (attendance == Is_Present) {
+
 			System.out.println("Employee is present");
-		else
+			int dailyWage = Wage_Per_Hour * Full_Day_Hour;
+			System.out.println("Employee dailywage is: " + dailyWage);
+		} else
 			System.out.println("Employee is absent");
 
 	}
