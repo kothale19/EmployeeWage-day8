c3a18aa533eeb138e35bff97828132077cfe79b2
kothale19 <kothale.pujaprasad@gmail.com>
2022-10-12 23:20:53 +0530

    Added: Compute employee wage by using class method

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -1,37 +1,38 @@
 package com.blz.employeewage;
 
 public class EmployeeWage {
-	static final int Is_Present = 1;
+	static final int Is_Full_Time = 1;
 	static final int Is_Parttime = 2;
 	static final int Wage_Per_Hour = 20;
 	static final int Full_Day_Hour = 8;
 	static final int Part_Time_Hour = 4;
 	static final int Working_Day_Per_Month = 20;
 	static final int Total_Work_Hour = 100;
-	static int totalHours = 0;
-	static int day = 0;
-	static int totalWage = 0;
-	static int dailyWage = 0;
+	private static final int IS_PART_TIME = 0;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program!!");
+	static void calculateWage() {
+		System.out.println("Welcome to Employee Wage Computation");
+		int totalHours = 0;
+		int day = 0;
+		int totalWage = 0;
+		int dailyWage = 0;
+
 		while ((totalHours < Total_Work_Hour) && (day < Working_Day_Per_Month)) {
-
 			int attendance = (int) (Math.floor(Math.random() * 10)) % 3;
+
 			switch (attendance) {
-			case Is_Present:
-				System.out.println("Employee is working Fulltime");
+
+			case Is_Full_Time:
 				dailyWage = Wage_Per_Hour * Full_Day_Hour;
 				totalHours += Full_Day_Hour;
 				break;
-			case Is_Parttime:
-				System.out.println("Employee is working Parttime");
+
+			case IS_PART_TIME:
 				dailyWage = Wage_Per_Hour * Part_Time_Hour;
 				totalHours += Part_Time_Hour;
 				break;
 			default:
-				System.out.println("Employee is absent");
-
+				break;
 			}
 			totalWage += dailyWage;
 			day++;
@@ -40,5 +41,9 @@ public class EmployeeWage {
 		System.out.println("Total work days are: " + day);
 		System.out.println("Total wage is: " + totalWage);
 
+	}
+
+	public static void main(String[] args) {
+		calculateWage();
 	}
 }
