14b14820bbf3891980179592fe9e70644fa3776b
kothale19 <kothale.pujaprasad@gmail.com>
2022-10-12 17:45:39 +0530

    Added: Employee present or absent logic

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -1,9 +1,16 @@
 package com.blz.employeewage;
 
 public class EmployeeWage {
+	static final int Is_Present = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program!!");
+		int attendance = (int) (Math.floor(Math.random() * 10)) % 2;
+		System.out.println(attendance);
+		if (attendance == Is_Present)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 
 	}
 
