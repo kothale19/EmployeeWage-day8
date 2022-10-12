40763c1869dfe749a24f115d703eb1dbdf57fc49
kothale19 <kothale.pujaprasad@gmail.com>
2022-10-12 18:08:41 +0530

    Added: Employee Parttime Wage Computation Logic

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -2,18 +2,24 @@ package com.blz.employeewage;
 
 public class EmployeeWage {
 	static final int Is_Present = 1;
+	static final int Is_Parttime = 2;
 	static final int Wage_Per_Hour = 20;
 	static final int Full_Day_Hour = 8;
+	static final int Part_Time_Hour = 4;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program!!");
-		int attendance = (int) (Math.floor(Math.random() * 10)) % 2;
-		System.out.println(attendance);
+		int attendance = (int) (Math.floor(Math.random() * 10)) % 3;
+//		System.out.println(attendance);
 		if (attendance == Is_Present) {
 
-			System.out.println("Employee is present");
+			System.out.println("Employee is working Fulltime");
 			int dailyWage = Wage_Per_Hour * Full_Day_Hour;
 			System.out.println("Employee dailywage is: " + dailyWage);
+		} else if (attendance == Is_Parttime) {
+			System.out.println("Employee is working Parttime");
+			int dailyWage = Wage_Per_Hour * Part_Time_Hour;
+			System.out.println("Employee daily wage is:" + dailyWage);
 		} else
 			System.out.println("Employee is absent");
 
